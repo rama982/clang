ad45b462eedc56da7213d030fd179e43e18148d6
rama982 <ramanarubp@gmail.com>
2022-01-14 05:13:56 +0000

    GenomTC: Bump to 20220114 build
    LLVM commit: https://github.com/llvm/llvm-project/commit/f5280553
    Clang Version: 14.0.0
    Binutils version: 2.37
    Builder commit: https://github.com/rama982/clang.git/commit/1ced55ab743eef1883b142666798c58165ff15a9
    
    Signed-off-by: rama982 <ramanarubp@gmail.com>

--- lib/cmake/llvm/LLVMConfig.cmake
+++ lib/cmake/llvm/LLVMConfig.cmake
@@ -1,6 +1,5 @@
 # This file provides information and services to the final user.
 
-
 # Compute the installation prefix from this LLVMConfig.cmake file location.
 get_filename_component(LLVM_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(LLVM_INSTALL_PREFIX "${LLVM_INSTALL_PREFIX}" PATH)
@@ -205,13 +204,17 @@ set(LLVM_APPEND_VC_REV "ON")
 # values for build tree.  LLVM_INCLUDE_DIRS contains both source
 # and generated include directories while the following variables have
 # them split.
-set(LLVM_INCLUDE_DIR "${LLVM_INSTALL_PREFIX}/include")
+
+# These are the "main" dirs
 set(LLVM_MAIN_INCLUDE_DIR "${LLVM_INSTALL_PREFIX}/include")
 set(LLVM_LIBRARY_DIR "${LLVM_INSTALL_PREFIX}/lib${LLVM_LIBDIR_SUFFIX}")
 
+# This is a secondary one for generated files
+set(LLVM_INCLUDE_DIR "${LLVM_INSTALL_PREFIX}/include")
+
 set(LLVM_DEFINITIONS "-D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS")
-set(LLVM_CMAKE_DIR "${LLVM_INSTALL_PREFIX}/lib/cmake/llvm")
 set(LLVM_BINARY_DIR "${LLVM_INSTALL_PREFIX}")
+set(LLVM_CMAKE_DIR "${LLVM_INSTALL_PREFIX}/lib/cmake/llvm")
 set(LLVM_TOOLS_BINARY_DIR "${LLVM_INSTALL_PREFIX}/bin")
 set(LLVM_TOOLS_INSTALL_DIR "bin")
 set(LLVM_HAVE_OPT_VIEWER_MODULES 0)
